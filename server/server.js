1cd9cd4f56896550c0afec70667c36b989a2779d
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-05-19 02:05:02 -0400

    Initializing server and static page

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//Middleware
+app.use(express.static(publicPath));
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${3000}`);
+});
