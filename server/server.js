011e5d9785ec22319350378648ad560f84dde78f
Temitayo Oyelowo <temitayooyelowo@gmail.com>
2018-05-19 09:10:27 -0400

    Add connect and disconnect event handlelrs

--- server/server.js
+++ server/server.js
@@ -1,14 +1,27 @@
 const path = require('path');
+const http = require('http'); //built in node module
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app); // http server instead of express server
+const io = socketIO(server);
 
 //Middleware
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//Do something when a client connects to the server
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('disconnect', (socket) => {
+    console.log('User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on port ${3000}`);
 });
